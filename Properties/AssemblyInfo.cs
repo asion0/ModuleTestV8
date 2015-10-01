f22db2d64154ebd32d98146601769b74f6d29d83
Alex Lin <alex.lin@skytraq.com.tw>
2015-10-01 09:48:46 +0800

    1.0.0.50

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,12 +32,10 @@ using System.Runtime.InteropServices;
 // 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將組建和修訂編號
 // 指定為預設值:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.48")]
-[assembly: AssemblyFileVersion("1.0.0.48")]
 
-// 1.0.0.48 - DR Module Test flow change. 20150709.
-// 1.0.0.47 - Add DR Module Test support. 20150625.
-// 1.0.0.46 - Support Winbond new flash and add new module name.
+[assembly: AssemblyVersion("1.0.0.45")]
+[assembly: AssemblyFileVersion("1.0.0.45")]
+
 // 1.0.0.45 - Fix report convert issue when continuous conversion.
 // 1.0.0.43 - Add write protected in download loader.
 // 1.0.0.42 - Add FactoryReset after download finished for flash ic write protected.
